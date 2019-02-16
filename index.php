e8a900e4b95c8609b0619159a856e6bdf4df448f
MavenProject_sample <43162712+mitunmbj@users.noreply.github.com>
2019-02-16 06:45:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo '<p>Hello ALL</p>'; ?>
+  <?php echo '<p>Welcome to zippyops</p>'; ?>
  </body>
 </html>
